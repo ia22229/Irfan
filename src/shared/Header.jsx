26a5cb78d6c19321cf3c0e3e46e3036beeff1884
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-04-28 12:09:08 +0530

    Header is created

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className='w-full h-[96px] px-[72px] bg-black flex justify-between items-center text-white text-xl font-medium'>
-        <div className="logo h-12 w-[100px] bg-white"></div>
-        <p>Home</p>
-        <p>Sub</p>
-        <p>HIW</p>
-        <p>FAQ</p>
-        <p>Referal</p>
-        <button className='px-12 py-3 bg-yellow-200 rounded-xl text-black font-semibold text-xl'>Login</button>
-    </header>
+    <div className="hidden md:flex justify-between items-center font-medium text-lg h-24 w-full bg-black  text-white z-50 px-[5%]">  
+    <div className="h-12 w-[100px] bg-white"></div>
+        <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>Home</span>
+        <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>Subscibe</span>
+        <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>How it works</span>
+        <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>FAQ</span>
+        <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>Referral</span>
+        <button className='h-[43px] w-[169px] bg-[#FEDE00] font-medium text-xl rounded-2xl text-black' >Login</button>
+    </div>
   )
 }
-
-export default Header
