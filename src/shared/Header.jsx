5b81ccdb5f4c15443b73ccf9fe21194980169c96
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-04-28 12:23:29 +0530

    Logo

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import logo from '../assets/images/shared/logo.png'
+
 function Header() {
   return (
     <div className="hidden md:flex justify-between items-center font-medium text-lg h-24 w-full bg-black  text-white z-50 px-[5%]">  
-    <div className="h-12 w-[100px] bg-white"></div>
+    <img src={logo} width={100} alt="" />
         <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>Home</span>
         <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>Subscibe</span>
         <span className='text-base font-semibold hover:text-[1.2rem] duration-200'>How it works</span>
